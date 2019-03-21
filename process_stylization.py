202a2e394933684d51ee75a2333b7d729a80c53d
Jaken <chenlong.lt@aliyun.com>
2019-03-21 11:26:19 +0800

    u

--- process_stylization.py
+++ process_stylization.py
@@ -57,13 +57,9 @@ def memory_limit_image_resize(cont_img):
     return cont_img.width, cont_img.height
 
 
-def stylization(stylization_module, smoothing_module, content_image_path, style_image_path, content_seg_path, style_seg_path, output_image_path,
-                cuda, save_intermediate, no_post, cont_seg_remapping=None, styl_seg_remapping=None):
+def stylization(stylization_module, smoothing_module, cont_img, styl_img, content_seg_path, style_seg_path, cuda, save_intermediate, no_post, cont_seg_remapping=None, styl_seg_remapping=None):
     # Load image
     with torch.no_grad():
-        cont_img = Image.open(content_image_path).convert('RGB')
-        styl_img = Image.open(style_image_path).convert('RGB')
-
     #    new_cw, new_ch = memory_limit_image_resize(cont_img)
     #    new_sw, new_sh = memory_limit_image_resize(styl_img)
         cont_pilimg = cont_img.copy()
@@ -96,23 +92,8 @@ def stylization(stylization_module, smoothing_module, content_image_path, style_
         if save_intermediate:
             with Timer("Elapsed time in stylization: %f"):
                 stylized_img = stylization_module.transform(cont_img, styl_img, cont_seg, styl_seg)
-            if ch != new_ch or cw != new_cw:
-                print("De-resize image: (%d,%d)->(%d,%d)" %(new_cw,new_ch,cw,ch))
-                stylized_img = nn.functional.upsample(stylized_img, size=(ch,cw), mode='bilinear')
             utils.save_image(stylized_img.data.cpu().float(), output_image_path, nrow=1, padding=0)
 
-            # with Timer("Elapsed time in propagation: %f"):
-            #    out_img = smoothing_module.process(output_image_path, content_image_path)
-            # out_img.save(output_image_path)
-
-            if not cuda:
-                print("NotImplemented: The CPU version of smooth filter has not been implemented currently.")
-                return
-
-#            if no_post is False:
-#                with Timer("Elapsed time in post processing: %f"):
-#                    out_img = smooth_filter(output_image_path, content_image_path, f_radius=15, f_edge=1e-1)
-#            out_img.save(output_image_path)
         else:
             with Timer("Elapsed time in stylization: %f"):
                 stylized_img = stylization_module.transform(cont_img, styl_img, cont_seg, styl_seg)
@@ -129,6 +110,5 @@ def stylization(stylization_module, smoothing_module, content_image_path, style_
 #            if no_post is False:
 #                with Timer("Elapsed time in post processing: %f"):
 #                    out_img = smooth_filter(out_img, cont_pilimg, f_radius=15, f_edge=1e-1)
-            # out_img.save(output_image_path)
             return out_img
 
--- views.py
+++ views.py
@@ -7,18 +7,37 @@ from PIL import Image
 import requests
 from io import BytesIO
 
+import torch
+import process_stylization
+from photo_wct import PhotoWCT
+from photo_gif import GIFSmoothing
+
+p_wct = PhotoWCT()
+p_wct.load_state_dict(torch.load('./PhotoWCTModels/photo_wct.pth'))
+# p_wct.cuda(0)
+p_pro = GIFSmoothing(r=35, eps=0.001)
+
 @api_view(['GET'])
 def style(request):
     try:
         params = request.GET
         url = params.get('url', '')
-        print(url)
-        content = Image.open(BytesIO(requests.get(url).content))
+        cont_img = Image.open(BytesIO(requests.get(url).content)).convert('RGB')
 
-        style=params.get('style', '1')
+        style = params.get('style', 'sunset')
+        styl_img = Image.open('./images/' + style + '.png').convert('RGB')
 
-        img = content
-
+        img = process_stylization.stylization(
+            stylization_module=p_wct,
+            smoothing_module=p_pro,
+            cont_img=cont_img,
+            styl_img=styl_img,
+            content_seg_path=[],
+            style_seg_path=[],
+            cuda=0,
+            save_intermediate=False,
+            no_post=False
+        )
         imgByteArr = BytesIO()
         img.save(imgByteArr, format='PNG')
         return HttpResponse(imgByteArr.getvalue(), content_type="image/png")
