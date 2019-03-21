f4aaf449533e7443d8274a12bf24ae07903e47bb
Jaken <chenlong.lt@aliyun.com>
2019-03-21 11:03:53 +0800

    u

--- views.py
+++ views.py
@@ -1,18 +1,11 @@
-from django.shortcuts import render
-from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
-from django.core import serializers
-from django.conf import settings
 
 from PIL import Image
 import requests
 from io import BytesIO
-
-# Create your views here.
 
 @api_view(['GET'])
 def style(request):
