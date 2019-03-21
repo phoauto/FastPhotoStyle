74500b7182016a175108e29870728feefa65d215
lire <154879786@qq.com>
2019-03-21 08:38:46 +0800

    u

--- demo_example1.sh
+++ demo_example1.sh
@@ -3,9 +3,9 @@ rm images/content1.png -rf;
 rm images/style1.png -rf;
 rm results/demo_result_example1.png
 cd images;
-axel -n 1 http://freebigpictures.com/wp-content/uploads/shady-forest.jpg --output=content1.png;
-axel -n 1 https://vignette.wikia.nocookie.net/strangerthings8338/images/e/e0/Wiki-background.jpeg/revision/latest?cb=20170522192233 --output=style1.png;
-convert -resize 25% content1.png content1.png;
+axel -n 1 https://pre00.deviantart.net/f1a6/th/pre/i/2010/019/0/e/country_road_hdr_by_mirre89.jpg --output=content1.png;
+axel -n 1 https://nerdist.com/wp-content/uploads/2017/11/Stranger_Things_S2_news_Images_V03-1024x481.jpg --output=style1.png;
+convert -resize 50% content1.png content1.png;
 convert -resize 50% style1.png style1.png;
 cd ..;
 python demo.py;
